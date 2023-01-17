9e68277442c282eb81a7a9dcedcb595bf7563d6e
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-17 18:33:15 -0300

    feat: função scrape_news - requisito 4 - raspa as informações e organiza em um dict

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from bs4 import BeautifulSoup
 
 
 # Requisito 1
@@ -39,6 +40,38 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_news(html_content):
     """Seu código deve vir aqui"""
+    s = Selector(text=html_content)
+    url = s.css("link[rel='canonical']::attr(href)").get()
+    title = str(s.css("h1.entry-title::text").get()).strip()
+    timestamp = s.css("li.meta-date::text").get()
+    writer = s.css("span.author a::text").get()
+
+    comments_count = s.css(".post-comments h5::text").re_first(r"\d+")
+    if type(comments_count) != str:
+        comments_count = 0
+    else:
+        comments_count = int(comments_count)
+
+    raw_summary = s.css(".entry-content > p").get()
+    soup = BeautifulSoup(raw_summary, 'html.parser')
+    for tag in soup.find_all(['strong', 'a', 'span', 'em']):
+        tag.replace_with(tag.text)
+    summary = soup.text.strip()
+
+    tags = s.css("a[rel='tag']::text").getall()
+    category = s.css(".entry-details span.label::text").get()
+
+    noticia = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
+    return noticia
 
 
 # Requisito 5
