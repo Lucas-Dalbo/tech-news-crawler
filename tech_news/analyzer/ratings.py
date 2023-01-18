b7a91d8c068c1e5c9666aff76a24390d37b022c7
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-01-18 11:22:07 -0300

    feat: função top_5_news - Retorna as 5 noticias com mais comentários

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,21 @@
+import operator
+from tech_news.analyzer.search_engine import news_tuplator
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    results = find_news()
+    news = sorted(
+        results, key=lambda results: results["title"]
+    )
+    sorted_by_comments = sorted(
+        news, key=lambda news: news["comments_count"]
+    )
+    sorted_by_comments.reverse()
+    top_news = sorted_by_comments[:5]
+    return news_tuplator(top_news)
 
 
 # Requisito 11
